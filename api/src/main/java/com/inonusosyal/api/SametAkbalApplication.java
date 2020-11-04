a30afdd9894515de9748b85af3a86a9d163e9b4d
Samet AKBAL <sametakbal>
2020-11-04 17:31:29 +0300

    upload

---
+++ api/src/main/java/com/inonusosyal/api/SametAkbalApplication.java
@@ -0,0 +1,13 @@
+package com.inonusosyal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SametAkbalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SametAkbalApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/inonusosyal/api/SametAkbalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inonusosyal.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SametAkbalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

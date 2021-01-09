44fb54dd7541c5bc62811d99bcae2833febc2a39
Samet AKBAL <sametakbal7@gmail.com>
2021-01-09 23:30:20 +0300

    UI Pages Create and BottomNavBarAdded

---
+++ mobileApp/lib/pages/post_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class PostPage extends StatefulWidget {
+  @override
+  _PostPageState createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('Will be Post Page'),
+      ),
+    );
+  }
+}
---
+++ mobileApp/lib/util/util.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const iconColor = Color(0xff6298fd);

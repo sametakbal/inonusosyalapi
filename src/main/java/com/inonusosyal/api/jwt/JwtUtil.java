8d758c5efdab53bb1b08b8a9d913cf5c4c6847b5
Samet AKBAL <samet@akbal.net>
2021-03-13 14:11:40 +0300

    Fix #3: Remove Token Expire Date

--- src/main/java/com/inonusosyal/api/jwt/JwtUtil.java
+++ src/main/java/com/inonusosyal/api/jwt/JwtUtil.java
@@ -16,14 +16,10 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String SECRET_KEY = "secret";
+    private final String SECRET_KEY = "secret";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
-    }
-
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
@@ -34,10 +30,6 @@ public class JwtUtil {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
 
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
     public String generateToken(UserEntity userEntity) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userEntity.getId().toString(),userEntity.getEmail());
@@ -46,12 +38,11 @@ public class JwtUtil {
     private String createToken(Map<String, Object> claims, String id, String subject) {
 
         return Jwts.builder().setClaims(claims).setId(id).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        return (username.equals(userDetails.getUsername()));
     }
 }

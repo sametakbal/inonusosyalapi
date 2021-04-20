99343dc6d3d31c69e259ddda56d19a20ad403d02
Samet AKBAL <samet@akbal.net>
2021-04-20 16:44:44 +0300

    Controllers Organized

---
+++ src/main/java/com/inonusosyal/api/controller/GroupController.java
@@ -0,0 +1,11 @@
+package com.inonusosyal.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/groups")
+@AllArgsConstructor
+public class GroupController {
+}
